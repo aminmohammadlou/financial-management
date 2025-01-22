de3b45d9185ace80c2093e61246bab307d5d73ea
aminmohammadlou99 <amin@ezpin.com>
2025-01-22 19:25:43 +0330

    Start designing register window

---
+++ UI/Views/Register.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI.Views
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton is MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
