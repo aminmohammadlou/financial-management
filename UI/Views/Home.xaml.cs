dd63010f3795610290236b40a0d1c82a7f3c93e8
aminmohammadlou99 <amin@ezpin.com>
2025-02-04 15:10:26 +0330

    Add home body contents

--- UI/Views/Home.xaml.cs
+++ UI/Views/Home.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using UI.Contents;
 
 namespace UI.Views
 {
@@ -19,15 +20,56 @@ namespace UI.Views
     /// </summary>
     public partial class Home : Window
     {
+        private TextBlock[] SideMenuButtonTexts { get; set; }
         public Home()
         {
             InitializeComponent();
+            SideMenuButtonTexts = [HomeButtonText, InvoicesButtonText, ProductsButtonText, CustomersButtonText];
         }
 
         private void Home_OnMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton is MouseButtonState.Pressed)
                 DragMove();
+        }
+
+        private void CustomersButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var customersContent = new HomeCustomersContent();
+            HomeBodyContent.Child = customersContent;
+
+            ChangeSideMenuButtonsColors(CustomersButtonText, "PurpleBrush");
+
+        }
+
+        private void ProductsButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var productsContent = new HomeProductsContent();
+            HomeBodyContent.Child = productsContent;
+
+            ChangeSideMenuButtonsColors(ProductsButtonText, "PurpleBrush");
+        }
+
+        private void InvoicesButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var invoicesContent = new HomeInvoicesContent();
+            HomeBodyContent.Child = invoicesContent;
+
+            ChangeSideMenuButtonsColors(InvoicesButtonText, "PurpleBrush");
+        }
+
+        private void ChangeSideMenuButtonsColors(TextBlock item, string colorName)
+        {
+            var brush = (Brush)FindResource(colorName);
+            item.Foreground = brush;
+
+            foreach (var sideMenuButtonText in SideMenuButtonTexts)
+            {
+                if (sideMenuButtonText == item)
+                    continue;
+                
+                sideMenuButtonText.Foreground = Brushes.White;
+            }
         }
     }
 }
